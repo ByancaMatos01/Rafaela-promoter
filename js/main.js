c71b6bb7873f6e14ce8b3e3b1e68abf442c44c12
Felype <132705654+FelypeDantas@users.noreply.github.com>
2024-04-14 19:20:08 -0300

    sininho ao adicionar ao carrinho

--- js/main.js
+++ js/main.js
@@ -4,13 +4,23 @@ let carousel = document.querySelector('.carousel');
 let listHTML = document.querySelector('.carousel .list');
 let seeMoreButtons = document.querySelectorAll('.seeMore');
 let backButton = document.getElementById('back');
+const botaoCarrinho = document.querySelectorAll('.add-carrinho');
+const addCarrinho = new Audio('./sons/item-pick-up-38258.mp3');
+
+botaoCarrinho.forEach((carrinho) => {
+    carrinho.onclick = function() {
+        addCarrinho.play();
+    }
+});
 
 nextButton.onclick = function(){
     showSlider('next');
 }
+
 prevButton.onclick = function(){
     showSlider('prev');
 }
+
 let unAcceppClick;
 const showSlider = (type) => {
     nextButton.style.pointerEvents = 'none';
@@ -31,12 +41,14 @@ const showSlider = (type) => {
         prevButton.style.pointerEvents = 'auto';
     }, 2000)
 }
+
 seeMoreButtons.forEach((button) => {
     button.onclick = function(){
         carousel.classList.remove('next', 'prev');
         carousel.classList.add('showDetail');
     }
 });
+
 backButton.onclick = function(){
     carousel.classList.remove('showDetail');
 }
